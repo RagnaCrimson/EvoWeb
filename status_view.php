145160b25e14dba0410f567d55a1a6cbdf57f145
RagnaCrimson <skylegoch003@gmail.com>
2024-07-10 15:51:54 +0700

    remove not use code

--- status_view.php
+++ status_view.php
@@ -168,16 +168,6 @@ $total_peak_per_month = $row_sum_peak_month['total_peak_per_month'];
                     <td><strong><?php echo $total_rows; ?></strong></td>
                 </tr>
             </table>
-            <nav aria-label="Page navigation">
-                <ul class="pagination">
-                    <?php 
-                    $total_pages = ceil($total_rows / $rows_per_page);
-                    for ($i = 1; $i <= $total_pages; $i++) {
-                        echo "<li class='page-item " . ($i === $page ? 'active' : '') . "'><a class='page-link' href='?page=$i&search=" . urlencode($search) . "'>$i</a></li>";
-                    }
-                    ?>
-                </ul>
-            </nav>
         </div>
     </div>
     <?php include 'back.html'; ?>
